45d6cfb2653c7659b7ec497f20bca71e9a517c89
LiuchangDuan <870121425@qq.com>
2016-11-02 23:55:06 +0800

    侧滑菜单

--- app/src/main/java/com/example/blogdemo/MainActivity.java
+++ app/src/main/java/com/example/blogdemo/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.blogdemo;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
+
+    // Fragment管理器
+    protected FragmentManager mFragmentManager;
+
+    // 除了ActionBar之外的根视图
+    private DrawerLayout mDrawerLayout;
+
+    // 菜单RecyclerView
+    private RecyclerView mMenuRecyclerView;
+
+    protected Toolbar mToolbar;
+
+    // ActionBar上的菜单开关
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFragmentManager = getFragmentManager();
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.app_name);
+
+        setSupportActionBar(mToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open, R.string.drawer_close);
+
+        mDrawerToggle.syncState();
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mMenuRecyclerView = (RecyclerView) findViewById(R.id.menu_recyclerview);
+
+        mMenuRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
     }
 
 }
